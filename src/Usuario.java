85a10cce5751c196f987d95ae1e94d049f863a56
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2024-12-23 19:01:36 -0300

    feat: implementa métodos da classe Usuário

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,25 +2,39 @@ import java.util.ArrayList;
 
 public class Usuario {
 
-    public String nome;
+    public String nomeDoUsuario;
     public int id;
     public ArrayList<Livro> livrosEmprestados;
 
     public Usuario (String nome, int id, ArrayList livrosEmprestados) {
-        this.nome = nome;
+        this.nomeDoUsuario = nome;
         this.id = id;
-        this.livrosEmprestados = livrosEmprestados;
+        this.livrosEmprestados = new ArrayList<>();
     }
 
     public void exibirDetalhes () {
-        System.out.println(Usuario.this);
+        System.out.println("Usuário: " + nomeDoUsuario + ". ID: " + id + ". Livros que estão emprestados para o usuário: " + livrosEmprestados);
     }
 
-    public void adicionarLivro () {
-        livrosEmprestados.add();
+    public boolean adicionarLivro (Livro livro) {
+        if(livrosEmprestados.size() < 3){
+            livrosEmprestados.add(livro);
+            System.out.println("O livro foi emprestado ao usuário!");
+            return true;
+        } else {
+            System.out.println("O usuário já possui o limite permitido de livros emprestados de uma única vez.");
+            return false;
+        }
     }
 
-    public void removerLivro () {
-        livrosEmprestados.remove();
+    public boolean removerLivro (Livro livro) {
+        if(livrosEmprestados.size() <= 0){
+            System.out.println("O livro não está emprestado para o usuário.");
+            return false;
+        } else {
+            livrosEmprestados.remove(livro);
+            System.out.println("O livro foi devolvido pelo usuário!");
+            return true;
+        }
     }
 }
